e41fbf055f8bc9958ba4e471e3d830437836b703
lukewire129 <lukewire129@gmail.com>
2025-05-21 15:41:10 +0900

    공통 State 모델 로직 적용

---
+++ src/FlexMVVM/State.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FlexMVVM
+{
+    public class State<T>: INotifyPropertyChanged
+    {
+        private T _value;
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (!Equals (_value, value))
+                {
+                    _value = value;
+                    OnPropertyChanged (nameof(Value));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
+
+        public State(T initialValue)
+        {
+            _value = initialValue;
+        }
+
+    }
+}
